23b2518ff367cf81042ece9a2cc13e4f4e29cc48
Bernie Elbourn <benri421@gmail.com>
2022-10-21 14:42:08 +0100

    v0.4 setBlockNetworkImage(false); supportZoom();

--- app/src/main/java/com/elbourn/android/whatsappweb/MainActivity.java
+++ app/src/main/java/com/elbourn/android/whatsappweb/MainActivity.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     WebView webView;
     String Url = "https://web.whatsapp.com";
-    String ua = "Mozilla/5.0 (Linux; Win64; x64; rv:46.0) Gecko/20100101 Firefox/68.0";
+    String ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,31 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         webView = (WebView) findViewById(R.id.webview);
-        webView.loadUrl(Url);
+
+        webView.setInitialScale(100);
         webView.setWebViewClient(new WebViewClient());
+//        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.getSettings().setUserAgentString(ua);
+        webView.getSettings().setAppCacheEnabled(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setUseWideViewPort(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.getSettings().setUserAgentString(ua);
         webView.getSettings().setGeolocationEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setDatabaseEnabled(true);
         webView.getSettings().setSupportMultipleWindows(true);
-        webView.getSettings().setAppCacheEnabled(true);
         webView.getSettings().setNeedInitialFocus(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        //webView.getSettings().setBlockNetworkLoads(true);
-        webView.getSettings().setBlockNetworkImage(true);
+        webView.getSettings().setBlockNetworkImage(false);
         webView.getSettings().setBuiltInZoomControls(true);
-        webView.setInitialScale(100);
         webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().supportZoom();
         webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.loadUrl(Url);
 
         Log.i(TAG, "end onCreate");
     }
-
 
     @Override
     protected void onResume() {
